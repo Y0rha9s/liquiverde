a5cc0148cfb86b97e62fbaf12f0512561c2727ea
Francisco Castro <fra.castroa@gmail.com>
2025-11-20 01:25:49 -0300

    fix pareto

--- frontend/src/ParetoChart.jsx
+++ frontend/src/ParetoChart.jsx
@@ -1,26 +1,30 @@
 import { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  BarController,
+  LineController,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 } from 'chart.js';
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  BarController,
+  LineController,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 import { productsAPI } from './api';
 
